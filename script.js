8d470f89dc1c45acacf3ced82d1374e2a3674264
enxel <26006596+enxel@users.noreply.github.com>
2025-07-23 18:15:14 -0600

    Do everything except keyboard support
    I know this is not a good commit, I honestly forgot to commit until
    I had already done most of the work.
    
    Note to self: Must practice "Commiting early and often" a lot more.

---
+++ script.js
@@ -0,0 +1,176 @@
+let operand1 = 0, operand2, operator;
+const display = document.querySelector(".display");
+let secondIsActive = false;
+let equalsExecuted = false;
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, op, num2) {
+    console.log("From operate: " + op);
+
+    let answer;
+
+    switch(op) {
+        case "+":
+            answer = add(num1, num2);
+            break;
+        case "-":
+            answer = substract(num1, num2);
+            break;
+        case "*":
+            answer = multiply(num1, num2);
+            break;
+        case "/":
+            if (num2 !== 0) {
+                answer = divide(num1, num2);
+                if (!Number.isInteger(answer)) {
+                    answer = Math.round(answer * 1000) / 1000;
+                }
+            } else {
+                alert("No, no, no... Don't be naughty!");
+                reset();
+                answer = 0;
+            }
+    }
+
+    return answer;
+}
+
+function handleClick(e) {
+    // console.log(e.target.textContent);
+    let txt = e.target.textContent;
+    if (!equalsExecuted) {
+        switch(txt) {
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                addOperand(txt);
+                break;
+            case "=":
+                processOperation(txt);
+                break;
+            case ".":
+                addPointToDisplay();
+                break;
+            case "<-":
+                backspace();
+                break;
+            case "clear":
+                reset();
+                break;
+            default: //digit
+                addDigitToDisplay(txt);
+        }
+    } else {
+        equalsExecuted = false;
+
+        display.textContent = "0";
+
+        if ("123456789".includes(txt)) {
+            addDigitToDisplay(txt);
+        }
+    }
+}
+
+function addDigitToDisplay(txt) {
+    if (display.textContent === "0") {
+        display.textContent = txt;
+    } else {
+        display.textContent += txt;
+    }
+
+    defineData();
+}
+
+function reset() {
+    display.textContent = "0";
+    operand1 = 0;
+    secondIsActive = false;
+}
+
+function backspace() {
+    if (display.textContent !== "0") {
+        if (display.textContent.length === 1) {
+            display.textContent = "0";
+        } else {
+            if (display.textContent.at(-1) === " ") {
+                display.textContent = display.textContent.slice(0, display.textContent.length - 3);
+            } else {
+                display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+                if (display.textContent.at(-1) === " ") {
+                    secondIsActive = false;
+                }
+            }
+        }
+    }
+
+    defineData();
+}
+
+function addPointToDisplay() {
+    if ( (!secondIsActive && !display.textContent.includes(".")) || (secondIsActive && !display.textContent.split(" ")[2].includes(".")) ) {
+        display.textContent += ".";
+    }
+}
+
+function addOperand(op) {
+    if (!secondIsActive) {
+        display.textContent += ` ${op} `;
+        secondIsActive = true;
+    } else {
+        if (display.textContent.at(-1) !== " ") {
+            processOperation("op");
+            display.textContent += ` ${op} `;
+            secondIsActive = true;
+        } else {
+            display.textContent = display.textContent.split(" ")[0] + ` ${op} `;
+        }
+    }
+}
+
+function defineData() {
+    if (!secondIsActive) {
+        if (display.textContent.at(-1) === " ") {
+            operand1 = Number(display.textContent.slice(0, display.textContent.length - 3));
+        } else {
+            operand1 = Number(display.textContent);
+        }
+        console.log(operand1);
+    } else {
+        operand2 = Number(display.textContent.split(" ")[2]);
+        console.log(operand1);
+        console.log(operand2);
+    }
+}
+
+function processOperation(from) {
+    if (secondIsActive) {
+        if (display.textContent.at(-1) !== " ") {
+            const result = operate(operand1, display.textContent.split(" ")[1], operand2);
+            display.textContent = `${result}`;
+            operand1 = result;
+            secondIsActive = false;
+            if (from === "=") {
+                equalsExecuted = true;
+            }
+        }
+    }
+}
+
+const btns = Array.from( document.querySelectorAll("button") );
+btns.forEach( item => { item.addEventListener("click", handleClick) } );
